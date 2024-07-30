835184a143627c3dc27b7f880bdf9983af90475b
RamziHkiri <6257@holbertonstudents.com>
2024-07-30 14:25:13 +0200

    fix prisma init

--- vardarproductionapp/libs/prismadb.ts
+++ vardarproductionapp/libs/prismadb.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma :PrismaClient;
+    var prisma: PrismaClient | undefined;
 }
 
 const client = globalThis.prisma || new PrismaClient();
@@ -10,4 +10,4 @@ if (process.env.NODE_ENV !== 'production') {
     globalThis.prisma = client;
 }
 
-export default client; 
+export default client;
