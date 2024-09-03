0d583a98ee7c39664c4b73786cc0fd0a1177bde5
RamziHkiri <6257@holbertonstudents.com>
2024-09-03 19:06:21 +0200

    add prospects files

--- vardarproductionapp/app/prospects/create/page.tsx
+++ vardarproductionapp/app/prospects/create/page.tsx
@@ -1,13 +1,12 @@
 import Container from '@/components/Container'
 import React from 'react'
-import AddFileProspects from '../components/AddFileProspects'
 import Title from '@/components/Title'
 
 export default function page() {
   return (
    <Container>
     <Title title='Ajouter un fichier des prospects'/>
-    <AddFileProspects/>
+
    </Container>
   )
 }
